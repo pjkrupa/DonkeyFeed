78d270be53b1fa262107a6366c7eea8b19e109ee
peter <pjkrupa@proton.me>
2024-01-11 22:39:48 +0100

    first commit!

--- RSS_parse.py
+++ RSS_parse.py
@@ -1,5 +1,38 @@
 import feedparser as fp
-from database import feeds
+import datetime
+
+class RSSfeed:
+    def __init__(self, rss_link):
+        self.rss_dict = fp.parse(rss_link)
+        self.feed_title = self.rss_dict.feed.title
+        self.feed_headlines = []
+        self.feed_links = []
+        self.feed_summaries = []
+        self.currentdate = datetime.datetime.now()
+
+    def process_feed(self):
+        for entry in self.rss_dict.entries:
+            entry_date = datetime.datetime(*entry.published_parsed[:6])
+            if 'published_parsed' in entry:
+                if (self.currentdate - entry_date).days < 5:
+                    self.feed_headlines.append(entry.title)
+                    self.feed_links.append(entry.link)
+                    if "summary" in entry:
+                        self.feed_summaries.append(entry.summary)
+                    else:
+                        self.feed_summaries.append("No summary available")
+
+    def headlines(self):
+        return self.feed_headlines
+
+    def links(self):
+        return self.feed_links
+
+    def summaries(self):
+        return self.feed_summaries
+
+
+
 
 def load_content(feeds):
     return_list = []
